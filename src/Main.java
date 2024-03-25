c40b81cf87b4a8dfa8bf684bcbc8f76584b2d550
Student <elkadjk@.pl>
2024-03-25 10:28:01 +0100

    pierwszy

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import javax.xml.xpath.XPath;
+import java.io.IOException;
+import java.util.Scanner;
+import java.nio.file.Path;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+//        Scanner scanner new Scanner(Path.of("test.txt"));
+//        int[] tab = {1,2,3};
+//        for (int i =0; i<10; i++){
+//            System.out.println(tab[i]);
+//        }
+
+        StosWyjatkow stosWyjatkow = new StosWyjatkow();
+        stosWyjatkow.metoda1();
     }
 }
---
+++ src/StosWyjatkow.java
@@ -0,0 +1,18 @@
+public class StosWyjatkow {
+    public void metoda1(){
+        System.out.println("Metoda 1");
+        metoda2();
+    }
+    public void metoda2(){
+        System.out.println("Metoda 2");
+        metoda3();
+    }
+    public void metoda3(){
+        System.out.println("Metoda 3");
+        metoda4();
+    }
+    public void metoda4(){
+        System.out.println("Metoda 4");
+    }
+
+}
