dc6d69749ee589ad789291ae13c47751e5f9ad6c
Student <elkadjk@.pl>
2024-04-08 09:33:19 +0200

    drugi

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 import javax.xml.xpath.XPath;
 import java.io.IOException;
 import java.util.IllegalFormatException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.nio.file.Path;
+import java.lang.Math;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -22,6 +24,30 @@ public class Main {
 //
 //        System.out.println("i tak was lubie");
 
-
+/*
+Napisz program, który pobierze od
+użytkownika liczbę i wyświetli jej pierwiastek.
+Do obliczenia pierwiastka możesz użyć istniejącej metody java.lang.Math.sqrt().
+ Jeśli użytkownik poda liczbę ujemną rzuć wyjątek java.lang.IllegalArgumentException.
+ Obsłuż sytuację, w której użytkownik poda ciąg znaków,
+który nie jest liczbą.
+ */
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj liczbe wieksza od zera: ");
+        try{
+            double user = scanner.nextDouble();
+            if (user <= 0){
+                throw new IllegalArgumentException("Nie mozna 0");
+            }
+            else {
+                System.out.println(Math.sqrt(user));
+            }
+        }
+        catch (IllegalFormatException e){
+            System.out.println("Podaj liczbe");
+        }
+        catch (InputMismatchException a){
+            System.out.println("zle");
+        }
     }
 }
